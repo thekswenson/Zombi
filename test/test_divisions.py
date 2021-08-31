80673f26fefabd9ce25c18d4b421763fe574463b
AAdavin <aaredav@gmail.com>
2021-08-31 19:39:09 +1000

    Adding test files

---
+++ test/test_divisions.py
@@ -0,0 +1,42 @@
+"""
+Unittests for testing the GenomeEvents and how they map coordinates.
+"""
+
+import os
+import unittest
+from zombi.Events import Inversion, LOSS, MapOriginError, MapPseudogeneError, ORIG, POS, TandemDup
+from zombi.GenomeSimulator import GenomeSimulator
+from zombi.Genomes import LEFT, RIGHT
+import zombi.AuxiliarFunctions as af
+
+
+GENOME_PARAMS = 'test/GenomeParametersDivisions.tsv'
+TEST_FOLDER = 'test/TestDivisions/'
+TEST_GENOME_30_6 = 'test/30_6.gff'  #30 bases, 5 * length-3 genomic/intergenomic pairs
+
+inject_events = list()
+
+event1 = ("G", 1.3, "I", "n2",(2 ,6,"RIGHT")) ## Events are a tuple where the elements are
+# 1. G or T (genome or tree level event)
+# 2. Time of the event
+# 3. Lineage undergoing the event
+# 4. Tuple with the details of the event
+
+inject_events.append(event1)
+
+class TestEvent(unittest.TestCase):
+
+  def setUp(self, genome_file=TEST_GENOME_30_6):
+    params = af.prepare_genome_parameters(af.read_parameters(GENOME_PARAMS))
+    events_file = os.path.join(TEST_FOLDER, 'T/Events.tsv')
+
+    self.gss = GenomeSimulator(params, events_file, genome_file)
+    self.gss.run_f_debug(inject_events)
+  
+  def test_ngenes(self):
+    
+    self.assertEqual(len(self.gss.all_genomes), 6) # We have six genomes
+
+
+if __name__ == '__main__':
+    unittest.main()
